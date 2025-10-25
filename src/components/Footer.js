f45c4b365fef47823c2713a5aaf8bfbdf088f965
Efe Eroğlu <210541019@firat.edu.tr>
2025-10-25 23:43:39 +0300

    Fix GitHub Pages routing: replace href with Link and add 404.html redirect

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import './Footer.css';
@@ -50,11 +51,11 @@ const Footer = () => {
           <motion.div className="footer-links" variants={itemVariants}>
             <h4>Quick Links</h4>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/projects">Projects</a></li>
-              <li><a href="/research">Research</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/research">Research</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
             </ul>
           </motion.div>
 
